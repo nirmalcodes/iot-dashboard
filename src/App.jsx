2b53033e102fecc582c03c5d3efcc748295554de
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2024-04-10 02:37:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import Layout from "./components/Layout";
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute/index.jsx';
+
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 const App = () => {
     return (
-        <>
-            <Layout>
-                app
-            </Layout>
-        </>
+        <Suspense fallback={<div>Loading...</div>}>
+            <ErrorBoundary fallback={<div>Something went wrong...</div>}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route
+                            path='/'
+                            element={
+                                <ProtectedRoute>
+                                    <Home />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route path='/signin' element={<SignIn />} />
+                        <Route path='/signup' element={<SignUp />} />
+                        <Route path='*' element={<div>Page Not Found</div>} />
+                    </Routes>
+                </BrowserRouter>
+            </ErrorBoundary>
+        </Suspense>
     );
 };
 
