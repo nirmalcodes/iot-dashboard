91fd219cdf8bbe44bd647d984ec24d8e35fe15bd
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2024-04-09 06:49:41 +0530

    Add navbar to layout component

---
+++ src/components/Layout/Navbar/index.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { FaBars, FaUser } from 'react-icons/fa6';
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(true);
+
+    const handleSidebarToggle = () => {
+        setIsOpen((prev) => !prev);
+    };
+
+    return (
+        <>
+            <header className={`navbar ${isOpen && 'sidebar--open'}`}>
+                <button
+                    type='button'
+                    onClick={handleSidebarToggle}
+                    className='mr-2 rounded-full p-2 leading-none transition-all duration-150 ease-in-out hover:bg-gray-100'
+                >
+                    <FaBars />
+                </button>
+                <div className='font-medium'>IOT Dashboard</div>
+                <div className='ml-auto'>
+                    <button
+                        type='button'
+                        className='rounded-full p-2 leading-none transition-all duration-150 ease-in-out hover:bg-gray-100'
+                    >
+                        <FaUser />
+                    </button>
+                </div>
+            </header>
+        </>
+    );
+};
+
+export default Navbar;
