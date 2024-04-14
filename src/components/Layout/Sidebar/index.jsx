d7ed066de57c101a10e939019b7e8c4e2b4bb23a
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2024-04-14 07:26:52 +0530

    Update sidebar component

--- src/components/Layout/Sidebar/index.jsx
+++ src/components/Layout/Sidebar/index.jsx
@@ -1,42 +1,70 @@
 import { Link, NavLink } from 'react-router-dom';
+import Collapsible from './Collapsible';
+import { useState } from 'react';
+import { FaChevronDown } from 'react-icons/fa6';
 
 const Sidebar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleCollapse = () => {
+        setIsOpen((prev) => !prev);
+    };
+
     return (
-        <>
-            <aside className='sidebar'>
-                <div className='sidebar__header' />
+        <aside className='sidebar'>
+            <div className='sidebar__header sticky top-0 border-b bg-white' />
 
-                <hr />
+            <ul className='sidebar__list'>
+                <li className='sidebar__list-item'>
+                    <NavLink to={'/'} className='sidebar__link'>
+                        First Link
+                    </NavLink>
+                </li>
+                <li className='sidebar__list-item'>
+                    <button
+                        type='button'
+                        onClick={toggleCollapse}
+                        className='sidebar__link flex items-center'
+                    >
+                        Link Btn
+                        <div
+                            className={`ml-auto transition-all duration-150 ease-out ${isOpen ? '-rotate-180' : ''}`}
+                        >
+                            <FaChevronDown />
+                        </div>
+                    </button>
 
-                <ul className='sidebar__list'>
-                    <li className='sidebar__list-item'>
-                        <NavLink to={'/'} className='sidebar__link'>
-                            Link
-                        </NavLink>
-                    </li>
-                    <li className='sidebar__list-item'>
-                        <NavLink to={'/1'} className='sidebar__link'>
-                            Link
-                        </NavLink>
-                    </li>
-                    <li className='sidebar__list-item'>
-                        <NavLink to={'/2'} className='sidebar__link'>
-                            Link
-                        </NavLink>
-                    </li>
-                    <li className='sidebar__list-item'>
-                        <NavLink to={'/3'} className='sidebar__link'>
-                            Link
-                        </NavLink>
-                    </li>
-                    <li className='sidebar__list-item'>
-                        <NavLink to={'/4'} className='sidebar__link'>
-                            Link
-                        </NavLink>
-                    </li>
-                </ul>
-            </aside>
-        </>
+                    <Collapsible open={isOpen}>
+                        <ul>
+                            <li>
+                                <NavLink
+                                    to={'/455'}
+                                    className='sidebar__link dropdown__link'
+                                >
+                                    Link
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to={'/test'}
+                                    className='sidebar__link dropdown__link'
+                                >
+                                    Link
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to={'/test'}
+                                    className='sidebar__link dropdown__link'
+                                >
+                                    Link
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </Collapsible>
+                </li>
+            </ul>
+        </aside>
     );
 };
 
